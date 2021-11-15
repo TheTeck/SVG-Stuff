de4c8eb0009a2da8e84e3859a186f23accce4f48
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-14 17:32:11 -0800

    Give svg some animation

--- script.js
+++ script.js
@@ -12,35 +12,50 @@ const eyebrowYOffset = -70;
 const g = svg.append('g')
     .attr('transform', `translate(${width/2}, ${height/2})`)
 
-const circle = g.append('circle')
+const circle = g
+  .append('circle')
     .attr('r', height / 2)
     .attr('fill', 'yellow')
     .attr('stroke', 'black');
 
-const eyesG = g.append('g')
-    .attr('transform', `translate(0, ${eyeYOffset})`);
+const eyesG = g
+  .append('g')
+    .attr('transform', `translate(0, ${eyebrowYOffset})`);
 
-const leftEye = eyesG.append('circle')
+const eyebrowsG = eyesG
+  .append('g')
+      .attr('transform', `translate(0, ${eyeYOffset})`);
+
+eyebrowsG
+  .transition().duration(2000)
+    .attr('transform', `translate(0, ${eyeYOffset - 30})`)
+  .transition().duration(2000)
+    .attr('transform', `translate(0, ${eyeYOffset})`)
+
+const leftEye = eyesG
+  .append('circle')
     .attr('r', eyeRadius)
     .attr('cx', -eyeSpacing)
 
-const rightEye = eyesG.append('circle')
+const rightEye = eyesG
+  .append('circle')
     .attr('r', eyeRadius)
     .attr('cx', eyeSpacing)
 
-const leftEyebrow = eyesG.append('rect')
+const leftEyebrow = eyebrowsG
+  .append('rect')
     .attr('x', -eyeSpacing - eyebrowWidth / 2)
-    .attr('y', eyebrowYOffset)
     .attr('width', eyebrowWidth)
     .attr('height', eyebrowHeight);
 
-const rightEyebrow = eyesG.append('rect')
-.attr('x', eyeSpacing - eyebrowWidth / 2)
-.attr('y', eyebrowYOffset)
-.attr('width', eyebrowWidth)
-.attr('height', eyebrowHeight);
+const rightEyebrow = eyebrowsG
+  .append('rect')
+    .attr('x', eyeSpacing - eyebrowWidth / 2)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight)
 
-const mouth = g.append('path')
+const mouth = g
+  .append('path')
     .attr('d', d3.arc()({
         innerRadius: 150,
         outerRadius: 170,
