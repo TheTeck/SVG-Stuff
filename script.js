dadaf42228c72ab71e88ef3ad1279461c320305f
John Teckemeyer <johnteckemeyer@gmail.com>
2021-11-14 16:12:06 -0800

    First Commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const svg = d3.select('svg');
+
+const height = +svg.attr('height');
+const width = +svg.attr('width');
+const eyeSpacing = 100;
+const eyeYOffset = -70;
+const eyeRadius = 30;
+const eyebrowWidth = 70;
+const eyebrowHeight = 15;
+const eyebrowYOffset = -70;
+
+const g = svg.append('g')
+    .attr('transform', `translate(${width/2}, ${height/2})`)
+
+const circle = g.append('circle')
+    .attr('r', height / 2)
+    .attr('fill', 'yellow')
+    .attr('stroke', 'black');
+
+const eyesG = g.append('g')
+    .attr('transform', `translate(0, ${eyeYOffset})`);
+
+const leftEye = eyesG.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', -eyeSpacing)
+
+const rightEye = eyesG.append('circle')
+    .attr('r', eyeRadius)
+    .attr('cx', eyeSpacing)
+
+const leftEyebrow = eyesG.append('rect')
+    .attr('x', -eyeSpacing - eyebrowWidth / 2)
+    .attr('y', eyebrowYOffset)
+    .attr('width', eyebrowWidth)
+    .attr('height', eyebrowHeight);
+
+const rightEyebrow = eyesG.append('rect')
+.attr('x', eyeSpacing - eyebrowWidth / 2)
+.attr('y', eyebrowYOffset)
+.attr('width', eyebrowWidth)
+.attr('height', eyebrowHeight);
+
+const mouth = g.append('path')
+    .attr('d', d3.arc()({
+        innerRadius: 150,
+        outerRadius: 170,
+        startAngle: Math.PI / 2,
+        endAngle:  3 * Math.PI / 2
+    }))
